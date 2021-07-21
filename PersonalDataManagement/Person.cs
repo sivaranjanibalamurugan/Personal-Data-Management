58a444d49a882bd54330b3c9d6be69613c731be6
unknown <sivaranjanib99@gmail.com>
2021-07-21 14:23:07 +0530

    [siva]UC1  creating a Personal Data Management

---
+++ PersonalDataManagement/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalDataManagement
+{
+    class Person
+    {
+        public int SSN { get; set; }
+        public int Age { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+    }
+}
---
+++ PersonalDataManagement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonalDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Personal Data Management!");
+            List<Person> list = new List<Person>();
+            AddingPersonDetails(list);
+
+
+        }
+        public static void AddingPersonDetails(List<Person> list)
+        {
+            list.Add(new Person() { SSN = 1, Name = "siva",    Age = 12, Address = "Bangaluru",  });
+            list.Add(new Person() { SSN = 2, Name = "Ranjani", Age = 14, Address = "kerela", });
+            list.Add(new Person() { SSN = 3, Name = "bala",    Age = 18, Address = "madurai",  });
+            list.Add(new Person() { SSN = 4, Name = "karthi",  Age = 65, Address = "chennai", });
+
+            foreach (Person person in list)
+            {
+                Console.WriteLine("SSn = {0}\tName={1}\tAddress = {2}\tAge={3}", person.SSN, person.Name, person.Address, person.Age);
+            }
+        }
+
+    }
+}
+
+
